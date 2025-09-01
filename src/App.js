81ef6fbcef4f4096b2c21d0e02dd2cc59b4773c1
onuyyy <qazxsw9026@naver.com>
2025-09-01 14:18:06 +0900

    feature : 예산 계산기 기본 페이지 세팅과 컴포넌트 구성

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import ExpenseForm from "./components/ExpenseForm";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <main className="main-container">
+        <h1>예산 계산기</h1>
+
+        <div style={{width:'100%', backgroundColor: 'white', padding: '1rem'}}>
+          <ExpenseForm />
+        </div>
+
+        <div style={{width:'100%', backgroundColor: 'white', padding: '1rem'}}>
+          {/*Expense List*/}
+        </div>
+
+        <div style={{ display: 'flex', justifyContent: 'end', marginTop: '1rem'}}>
+          <p style={{ fontSize: '2rem'}}>
+            총 지출:
+            <span>원</span>
+          </p>
+        </div>
+      </main>
+    )
+  }
 }
 
+
+// 내보내줘야 index.js 에서 사용할 때 가져와서 사용할 수 있다.
+// default : 메인으로 내보낸다는 뜻
 export default App;
---
+++ src/components/ExpenseList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class ExpenseList extends Component {
+  render() {
+    return (
+      <div>ExpenseList</div>
+    )
+  }
+}
+
+export default ExpenseList
